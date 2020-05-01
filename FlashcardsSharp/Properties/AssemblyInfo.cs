b7a1f88d9802adbdf8dd7ae31e50e9057df186a5
Darian Benam <darianbenam@hotmail.com>
2020-05-01 15:19:27 -0400

    Added in assembly information

--- FlashcardsSharp/Properties/AssemblyInfo.cs
+++ FlashcardsSharp/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FlashcardsSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A program to create flashcard sets for study purposes.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FlashcardsSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Darian Benam 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
